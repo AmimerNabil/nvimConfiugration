cfa8c4fdbd0c3c61455bc66484dcb211c0f0a3ad
izeLHL <nabil.amimer@mail.mcgill.ca>
2024-03-09 10:17:40 -0500

    updated vim config with folding

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    shade_terminals = true,
+    shell = "~/../../Program Files/Git/bin/bash.exe",
+    open_mapping = [[<\\>]],
+    start_in_insert = true,
+    hide_numbers = true,
+    direction = "horizontal",
+  },
+}
